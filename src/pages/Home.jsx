0a83564d20bc1f56c1beb057bdef68b38ab189ea
Mohit Singh <mohitbanna085@gmail.com>
2026-02-23 12:30:59 +0530

    update

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import HomeBanner from '../components/HomeBanner'
+import HomeCars from '../components/HomeCars'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <HomeBanner/>
+        <HomeCars/>
+    </div>
+  )
+}
+
+export default Home
